fa280b3c7b2c7023cff7bbba1c8752b97745caf7
Thiago <thiagomarcial1109@gmail.com>
2025-01-15 22:57:29 -0300

    adding responsive design and removing tailwind css

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const label = document.getElementById("menu-open");
+const nav = document.querySelector('#nav');
+const links = document.querySelectorAll('#nav a')
+
+label.addEventListener("click", () => {
+    nav.classList.toggle("visible");
+})
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove('visible')
+    })
+});
